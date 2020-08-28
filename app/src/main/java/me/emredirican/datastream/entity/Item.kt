e1a9ff9047a1ac22df57ec4b4c2c6f23e9b4190e
Emre Dirican <emredirican@gmail.com>
2020-08-28 22:42:42 +0300

    Define Item entity

---
+++ app/src/main/java/me/emredirican/datastream/entity/Item.kt
@@ -0,0 +1,23 @@
+package me.emredirican.datastream.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class Item(
+    val browser: Browser,
+    @SerializedName("geo")
+    val geoLocation: GeoLocation,
+    val rating: Long,
+    val labels: Set<String>
+)
+
+data class Browser(
+    val version: String,
+    val platform: String
+)
+
+data class GeoLocation(
+    @SerializedName("lat")
+    val latitude: Double,
+    @SerializedName("lon")
+    val longitude: Double
+)
